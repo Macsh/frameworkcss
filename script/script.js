3c46fe695389af2adbd2629abb4fc01bc7a6872e
Macsh <a.menskoi@gmail.com>
2022-02-15 10:47:44 +0100

    Update before merge

--- script/script.js
+++ script/script.js
@@ -20,3 +20,29 @@ $('.dropdown-menu li').hover(function() {
         $(this).toggleClass('cursor-pointer')
     }
 });
+
+$('.navbar-toggler').on('click', function(){
+    if($(".navbar-nav").is(":visible")){
+        $(".navbar-nav").css("display", "none");
+    }
+    else {
+        $(".navbar-nav").css("display", "block");
+    }
+    if($("form").is(":visible")){
+        $("form").css("display", "none");
+    }
+    else {
+        $("form").css("display", "block");
+    }
+});
+
+$(window).on('resize', function(){
+    if(!$(".navbar-toggler").is(":visible")){
+        $(".navbar-nav").css("display", "flex");
+        $("form").css("display", "flex");
+    }
+    else{
+        $(".navbar-nav").css("display", "none");
+        $("form").css("display", "none");
+    }
+});
